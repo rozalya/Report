64f8b98dd73e22914a27d925aac462bee7c66ed7
Rozaliya Evtimova <rosalia_evtimova@abv.bg>
2020-10-02 07:44:44 +0300

    Add project files.

---
+++ MyReport.Tests/Hooks1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+using TechTalk.SpecFlow;
+
+namespace MyReport.Tests
+{
+    [Binding]
+    public sealed class Hooks1
+    {
+        private static Process _iisExpressProcess;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            StartIISExpress();
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            StopIISExpress();
+        }
+
+        private static void StartIISExpress()
+        {
+            var arguments = string.Format(
+                CultureInfo.InvariantCulture,
+                "/path:\"{0}\" /port:{1}", "https://localhost:44310/", 44310);
+
+            var startInfo = new ProcessStartInfo(@"C:\Users\rosal\source\repos\MyReport\MyReport")
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                ErrorDialog = true,
+                LoadUserProfile = true,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                Arguments = arguments
+            };
+
+            _iisExpressProcess = Process.Start(startInfo);
+        }
+
+        private static void StopIISExpress()
+        {
+            if (_iisExpressProcess.HasExited == false)
+            {
+                _iisExpressProcess.Kill();
+                _iisExpressProcess.Dispose();
+                _iisExpressProcess = null;
+            }
+        }
+    }
+}
---
+++ MyReport.Tests/SpecFlowFeature1Steps.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace MyReport.Tests
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            Thread.Sleep(1000);
+        }
+        
+        
+    }
+}
